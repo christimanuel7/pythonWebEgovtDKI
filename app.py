faaa1b1a3ac996630860eecc0d9c677321349e76
Christopher Imanuel <christ.imanuel7@gmail.com>
2022-12-21 22:34:35 +0700

    Rabu, 21 Des 2022 22:34

--- app.py
+++ app.py
@@ -26,5 +26,25 @@ def berita3():
 def berita4():
     return render_template('berita4.html')
 
+@app.route('/aduanmasyarakat')
+def aduanmasyarakat():
+    return render_template('aduanmasyarakat.html') 
+
+@app.route('/profildki/sejarah')
+def sejarahdki():
+    return render_template('sejarahdki.html')
+
+@app.route('/profildki/visimisi')
+def visimisidki():
+    return render_template('visimisidki.html')
+
+@app.route('/profildki/pimpinan')
+def pimpinandki():
+    return render_template('pimpinandki.html') 
+
+@app.route('/profildki/kantor')
+def alamatdki():
+    return render_template('alamatdki.html')  
+
 if __name__ == '__main__':
     app.run(debug=True)
